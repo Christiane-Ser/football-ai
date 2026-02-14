ac4d331dac32daccc60afc3b619369a716c04745
Adrienhm <adrienfodjo@yahoo.com>
2026-02-14 10:46:25 +0100

    routes(match): exposer les endpoints sports et statistiques

--- backend/routes/match.routes.js
+++ backend/routes/match.routes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   createMatch,
-  getMatches
+  getMatches,
+  getMatchStats,
+  getSupportedSports,
 } = require("../controllers/match.controller");
 
 router.post("/", createMatch);
 router.get("/", getMatches);
+router.get("/sports", getSupportedSports);
+router.get("/stats", getMatchStats);
 
 module.exports = router;
